14ed952f7594d8d50fb1f485eb651ec1e430eaac
ashwanisahu97 <ashsahu627@gmail.com>
2023-01-11 23:27:53 +0530

    showing random image on BreedDetails page

--- src/Pages/BreedDetails.jsx
+++ src/Pages/BreedDetails.jsx
@@ -17,8 +17,10 @@ const BreedDetails = () => {
        setImage(data.message)
     }
     return (
-        <div style={{display:"grid",gridTemplateColumns:repeat(5,1fr)}}>
-            <img src={ image} alt="breed random image" />
+        <div style={{ display: "flex",alignItems:"center",justifyContent:"center" }}>
+            <div style={{width:"400px",height:"400px",border:"2px solid red"}}>
+<img width="100%" height="100%" src={ image} alt="breed random image" />
+            </div>
         </div>
     //   <div>{ JSON.stringify(image)}</div>
   )
